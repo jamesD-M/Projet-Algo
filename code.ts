a82cca6a253609e29b2b25fcbb4787ba94117c4e
jamesD-M <james.djeugoue.munezero@gmail.com>
2025-09-22 21:53:34 +0200

    feat: création de la class Piece docs: pseudocode répresentant l'initialisation du plateau et le placement des pieces

--- code.ts
+++ code.ts
@@ -1,2 +1,19 @@
 // Projet: Création du jeu de Dames
-//
+// 1. Création de classe: pièces
+export class Piece {
+    joueur: string; // "R" (rouge) ou "B" (bleu/noir)
+    estDame: boolean;
+
+    constructor(joueur: string, estDame: boolean = false) {
+        this.joueur = joueur;
+        this.estDame = estDame;
+    }
+
+    // Obtenir une représentation textuelle
+    affichage(): string {
+        if (this.estDame) {
+            return this.joueur.toUpperCase(); // R ou B
+        }
+        return this.joueur.toLowerCase(); // r ou b
+    }
+}
